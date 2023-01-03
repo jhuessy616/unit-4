d6da56424a19706699726a5f1414d1bc0819b908
Julia Huessy <jhuessy@hotmail.com>
2023-01-02 23:16:40 -0500

    changes made with rob while reviewing over the break

--- challenges/hellofrenemyurl/greeting.js
+++ challenges/hellofrenemyurl/greeting.js
@@ -1,11 +1,9 @@
-let frienGreeting = document.getElementById("friend-greeting");
-
 let friendGreeting = document.getElementById("friend-greeting");
 
 let queryString = window.location.search;
-
+console.log(queryString)
 let params = new URLSearchParams(queryString);
-
+console.log(params)
 let friendName = params.get("name");
-
+console.log(friendName)
 friendGreeting.textContent = "Hello " + friendName;
--- challenges/hellofrenemyurl/index.js
+++ challenges/hellofrenemyurl/index.js
@@ -18,15 +18,19 @@ submitBtn.addEventListener("click", (event) => {
   let filteredList = enemies.filter((enemy) => {
     // Darth Vader from the array
     // Darth from the input
+enemy.toLowerCase()=== input.value.toLowerCase()
+
     let name = enemy.toLowerCase().split(" ");
     if (name.indexOf(input.value.toLowerCase()) > -1) {
-      return enemy;
+      return true;
+      // previously had return enemy but filter is checking if true or false and then deciding whether to keep it based on whether true or false.
     }
   });
-  let result = enemies.find((enemy) =>
-    enemy.toLowerCase().includes(input.value.toLowerCase())
-  );
-  console.log(result);
+  // !What Paul had but the issue with includes is if a name is part of a larger enemy name 
+  // let result = enemies.find((enemy) =>
+  //   enemy.toLowerCase().includes(input.value.toLowerCase())
+  // );
+  // console.log(result);
 
   if (filteredList.length > 0) {
     response.textContent = `Go Away ${input.value}`;
